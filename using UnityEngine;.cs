b91d1c42ef5a0d57aa6052f50b01a60ec0bed803
helloasaf <huseyinasafbolat@gmail.com>
2023-10-15 20:34:08 +0300

    Anladım
    blablabla

---
+++ using UnityEngine;.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    public Button[] buttons; // 12 butonunuzu bu diziye ekleyin.
+
+    private bool colorChanged = false;
+    private int Number;
+
+    private void Start()
+    {
+        
+    }
+}
